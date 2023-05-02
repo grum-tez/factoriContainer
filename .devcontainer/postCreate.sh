e43b7b56fb56047a6aa430a58ef2c0fabe5e571a
grum-tez <grum.tez@gmail.com>
2023-05-02 04:46:36 +0000

    initial commit

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt-get update && export DEBIAN_FRONTEND=noninteractive \
+&& sudo apt-get install -y software-properties-common gnupg \
+&& sudo add-apt-repository -y 'deb http://ppa.launchpad.net/serokell/tezos/ubuntu bionic main' \
+&& sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 37B8819B7D0D183812DCA9A8CE5A4D8933AE7CBB \
+&& sudo apt-get update -y \
+&& sudo apt-get install -y tezos-client \
+&& wget https://gitlab.com/functori/dev/factori/-/snippets/2509259/raw/main/factori.sh -O factori \
+&& chmod +x factori \
+&& sudo mv factori /usr/bin/
+
+
